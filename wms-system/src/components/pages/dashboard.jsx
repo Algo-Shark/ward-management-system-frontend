06cdf23116cc3c99911ba768ad1c52529678a061
janath <janathhma@gmail.com>
2024-01-29 15:50:15 +0530

    add corrections

--- wms-system/src/components/pages/dashboard.jsx
+++ wms-system/src/components/pages/dashboard.jsx
@@ -1,38 +1,37 @@
 import React from "react";
 import Card from "../common/Card";
 
-
 function Dashboard() {
   return (
     <>
-      <div className="container-fluid">
-        <div className="col-md-9">
-          {/* First row clock */}
-          <div className="row">
-            <div className="col-md-4 ml-auto">
-              
+      <div className="container">
+        <div className="row">
+          <div className="col-md-9">
+            {/* First row clock */}
+            <div className="row">
+              <div className="col-md-4 ml-auto"></div>
             </div>
-          </div>
 
-          {/* Second row three cards */}
-          <div className="row">
-            <div className="col-md-4">
-              <Card name="Doctor count" count={6} />
+            {/* Second row three cards */}
+            <div className="row">
+              <div className="col-md-4">
+                <Card name="Doctor count" count={6} />
+              </div>
+              <div className="col-md-4">
+                <Card name="Nurse count" count={65} />
+              </div>
+              <div className="col-md-4">
+                <Card name="Patient count" count={45} />
+              </div>
             </div>
-            <div className="col-md-4">
-              <Card name="Nurse count" count={65} />
-            </div>
-            <div className="col-md-4">
-              <Card name="Patient count" count={45} />
-            </div>
-          </div>
 
-          {/* Third row with one card and calender */}
-          <div className="row">
-            <div className="col-md-4 col-sm-6">
-              <Card name="Appoinment Count" count={45} />
+            {/* Third row with one card and calender */}
+            <div className="row">
+              <div className="col-md-4 col-sm-6">
+                <Card name="Appoinment Count" count={45} />
+              </div>
+              <div className="col-md-6 col-sm-6 ml-auto"></div>
             </div>
-            <div className="col-md-6 col-sm-6 ml-auto"></div>
           </div>
         </div>
       </div>
