5df6bafeac1de914365ae2b729df4a11d173936e
Dashan Nadeema <dashanwd@gmail.com>
2024-03-03 18:15:14 +0530

    login page new view completed

--- wms-system/src/App.jsx
+++ wms-system/src/App.jsx
@@ -1,12 +1,20 @@
 import { useState } from 'react'
 import './App.css'
-
-
+import Login from "./pages/Login"
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
  
   return (
     <>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Login/>}/> 
+        {/* <Route path='/dashboard' element={<About/>}></Route>
+        <Route path='/vans' element={<Vans/>}></Route>
+        <Route path='vans/:id' element={<VansDetails/>}></Route> */}
+       </Routes>
+    </BrowserRouter> 
 
     </>
   )
---
+++ wms-system/src/pages/Login.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import Video from "../assets/img/video.mp4"
+import Image from "../assets/img/Logo.png"
+import "./Login.css"
+import { Link } from "react-router-dom";
+import  Axios  from "axios";
+
+
+export default function Login() {
+    const url=""
+    const [formData,setFormData]=useState({
+        username:"",
+        password:""
+    })
+
+    function handleChange(e){
+        setFormData((prevState)=>{
+            return{
+                ...prevState,
+                [e.target.name]:e.target.value
+            }
+        })
+    }
+    function submitForm(e){
+        e.preventDefault()
+        Axios.post(url,{username:formData.username,password:formData.password})
+        .then((res)=>{
+            //need to write after the backend is completed
+        })
+    }
+    return (
+        <div className="container-login">
+            <video id="image-login" src={Video} width="400px" autoPlay muted loop />
+            
+            <div className="content-login">
+                <div className="heading-login">
+                    <h1 id="heading-login-main">Panadura Nursing Home</h1>
+                    <h2 id="login-heading">Login</h2>
+                </div>
+
+                <div className="form-content">
+                    <form id="login-form">
+                        <label htmlFor="username">Username</label>
+                        <input className="input-login" type="text" placeholder="Email or Phone" required name="username" onChange={(e)=>handleChange(e)} />
+                        <label htmlFor="password">Password</label>
+                        <input className="input-login" type="password" placeholder="Password" required name="password" onChange={(e)=>handleChange(e)}/>
+                        <Link to="/forgot">
+                        <p id="forgot-password">Forgot password click here</p>
+                        </Link>
+                        <Link to="/Dashboard">
+                        <button id="btn-login" onClick={(e)=>submitForm(e)}>Log In</button>
+                        </Link>
+                    </form>
+                </div>
+            </div>
+            <img id="logo" src={Image} width="100px" />
+        </div>
+
+    )
+}
