937caa61294fb8997bd163c6e71ce6ccbb0f4fc4
Dashan Nadeema <dashanwd@gmail.com>
2024-03-05 15:04:12 +0530

    patient view completed!

--- wms-system/src/components/pages/PatientView.jsx
+++ wms-system/src/components/pages/PatientView.jsx
@@ -3,6 +3,7 @@ import SideBar from "../../pages/SideBar";
 import { Link, useParams } from "react-router-dom"
 import Data from "../../../../Data";
 import "./patientView.css"
+import video from "../../assets/img/bgvideo.mp4"
 
 export default function PatientView() {
     const { ward, bed } = useParams()
@@ -20,17 +21,53 @@ export default function PatientView() {
                         <p id="back-btn-patient-info">Back</p>
                     </Link>
                 </div>
+                <div className="heading-image">
+                    <video id="image-heading" src={video} width="800px" height="100px" autoPlay muted loop/>
+                </div>
                 <div className="name-container">
-                    <h3>Dashan Nadeema Wickramasuriya</h3>
-                    <p>Patient name</p>
+                    <h3 id="patient-name">Dashan Nadeema Wickramasuriya</h3>
+                    <p className="para-patient-view">Patient name</p>
                 </div>
-                <div className="ward-number">
-                    <h5>{ward}</h5>
-                    <p>Name of the ward</p>
+                <div className="secondary-container">
+                    <div className="ward-number">
+                        <h5 id="patient-name">{ward}</h5>
+                        <p className="para-patient-view">Name of the ward</p>
+                    </div>
+                    <div className="bed-number">
+                        <h5 id="patient-name">{bed}</h5>
+                        <p className="para-patient-view">Number of the bed</p>
+                    </div>
                 </div>
-                <div className="bed-number">
-                    {/* <h5>{bed}</h5>                      add a div to the both of them  */}
-                    <p>Number of the bed</p>
+                <div className="personal-info">
+                    <div className="patient-age">
+                        <h5 id="patient-name">20</h5>
+                        <p className="para-patient-view">Age</p>
+                    </div>
+                    <div className="patient-gender">
+                        <h5 id="patient-name">Male</h5>
+                        <p className="para-patient-view">Gender</p>
+                    </div>
+                    <div className="patient-nationality">
+                        <h5 id="patient-name">Sri lankan</h5>
+                        <p className="para-patient-view">Nationality</p>
+                    </div>
+                </div>
+                <div className="medical-info">
+                    <div className="heading-diagonosis">
+                        <h5 id="patient-name">Diagonosis</h5>
+                        <p className="info">Here the patient diagonosis will be added to the ejgfioshgirebgrb
+                            ifgriyfgv iuguidfgviudfguifdhiufsgiugfdckvbdiufguvidu hreiuhjkbdfus
+                            ydvnbvdiflojhbiofdhbioerhgv iodfhgiofuh iojhgkjfhjbfdhjb gfjo rhfb
+                            jkfhguobgfuihukfdg uifg</p>
+                    </div>
+                </div>
+                <div className="heading-medication">
+                    <h5 id="patient-name">Medications</h5>
+                    <p className="info">Medications related to the patient can be viewed in here and also ydvnbvdiflojhbiofdhbioerhgv iodfhgiofuh iojhgkjfhjbfdhjb gfjo rhfb  </p>
+                </div>
+                <div className="heading-medication">
+                    <h5 id="patient-name">Instructions</h5>
+                    <p className="info">Medications related to the patient can be viewed in here and also ydvnbvdiflojhbiofdhbioerhgv iodfhgiofuh iojhgkjfhjbfdhjb gfjo rhfb  </p>
                 </div>
             </div>
 
